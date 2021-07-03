fb5124e65e9feb06b022e9dd27deab280caf6f77
BuildTools <mattieu.vendeville@gmail.com>
2021-07-03 16:08:15 +0200

    update Results component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,6 +1,35 @@
 <template>
     <v-row>
-        <p>results</p>
+        <v-col col="12" align="center" v-if="more == false">
+            <div class="img_container">
+                <img src="../assets/podium.png" alt="podium">
+            </div>
+            <div class="cat_container first">
+                <img src="../assets/cat_1.jpg" alt="cat_1">
+                <p>Votes : 985474</p>
+            </div>
+            <div class="cat_container second">
+                <img src="../assets/cat_2.jpg" alt="cat_2">
+                <p>Votes : 98547</p>
+            </div>
+            <div class="cat_container third">
+                <img src="../assets/cat_3.jpg" alt="cat_3">
+                <p>Votes : 98547</p>
+            </div>
+            <div class="more">
+                <button @click="seeMore(true)">Voir les autres chats</button>
+            </div>
+        </v-col>
+        <v-col col="12" v-if="more == true">
+            <v-row>
+                <v-col align="center">
+                    <div class="cat_container_more">
+                    <img src="../assets/cat_4.jpg" alt="cat_4">
+                        <p>Votes : 98547</p>
+                    </div>
+                </v-col>
+            </v-row>
+        </v-col>
     </v-row>
 </template>
 
@@ -10,9 +39,79 @@ export default {
   props: {
     // msg: String,
   },
+  data () {
+      return{
+          more: false
+      }
+  },
+  methods: {
+      seeMore: function(more) {
+        this.more = more;
+      }
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    .img_container{
+        height: 20rem;
+        margin-top: 15rem;
+        position: relative;
+        top: 0;
+    }
+
+    .img_container img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    .cat_container{
+        width: 14rem;
+        height: 14rem;
+        margin-top: 5rem;
+    }
+
+    .cat_container img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 100%;
+    }
+
+    .first{
+        position: relative;
+        top: -34rem;
+    }
+
+    .second{
+        position: relative;
+        top: -50rem;
+        right: 16.5rem;
+    }
+
+    .third{
+        position: relative;
+        top: -68rem;
+        left: 16rem;
+    }
+
+    .more{
+        position: relative;
+        top: -50rem;
+    }
+
+    .cat_container_more{
+        width: 8rem;
+        height: 8rem;
+        margin-top: 5rem;
+    }
+
+    .cat_container_more img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 100%;
+    }
 </style>
