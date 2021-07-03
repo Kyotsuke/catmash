a86e25f46d6c37465239c2a99c0ca7f145f4c72e
BuildTools <mattieu.vendeville@gmail.com>
2021-07-03 14:31:20 +0200

    add Results component in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <v-container>
       <Navbar />
-      <CatMash />
+      <CatMash v-if="results != true" />
+      <Results v-if="results != false" />
   </v-container>
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
 import CatMash from "../components/CatMash";
+import Results from "../components/Results";
 
 export default {
   name: "Home",
 
   components: {
     Navbar,
-    CatMash
+    CatMash,
+    Results
   },
+  data () {
+    return {
+      results: true
+    }
+  }
 };
 </script>
 
